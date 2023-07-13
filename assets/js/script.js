8f9ae20b761c5ebfe3ae7fd88e48b4e40055dcd6
Mário <marionorberto2018@gmail.com>
2023-07-13 23:08:10 +0100

    projecto final

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,15 @@ const second = document.querySelector('.second');
 
 
 function setTime(){
-    const data = new Date();
-    setInterval(function(){
+    
+    const time = setInterval(function(){
+        const data = new Date();
         hour.innerText = (data.getHours() < 10) ? `0${data.getHours()}`: data.getHours();
         minute.innerText = (data.getMinutes() < 10) ? `0${data.getMinutes()}` : data.getMinutes();
         second.innerText = (data.getSeconds() < 10) ? `0${data.getSeconds()}` : data.getSeconds();
-    },1000); 
+    },1000);
+    
+    time();
 }
 
 setTime();
